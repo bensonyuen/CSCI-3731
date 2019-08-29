e16cefec2f0677ea1a87a275b3c8fcea49598c1f
Benson Yuen <byuen@my.hpu.edu>
2019-08-28 16:03:17 -1000

    HW01

---
+++ hw01/add.h
@@ -0,0 +1,9 @@
+// these preprocessor directives are a guard against this header filed
+// being included twice
+#ifndef ADD_H
+#define ADD_H
+
+// this is the declaration of the add function
+double add(double x);
+
+#endif
---
+++ hw01/main.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+#include "add.h"
+
+/************************************************************************
+ *
+ ************************************************************************/
+int main(int argc, char** argv) {
+    
+    // here we call the sqared function in about as simple an example program
+    // as you can get
+    printf("%lf\n", add(3.0));
+    
+} // end of main
+
