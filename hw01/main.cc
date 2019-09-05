3fec722b58255196cc7473cc5bda47d0ff95e687
Benson Yuen <byuen@my.hpu.edu>
2019-09-04 16:17:15 -1000

    test commit

--- hw01/main.cc
+++ hw01/main.cc
@@ -7,8 +7,6 @@
  ************************************************************************/
 int main(int argc, char** argv) {
     
-    // here we call the sqared function in about as simple an example program
-    // as you can get
     printf("%lf\n", add(3.0));
     
 } // end of main
