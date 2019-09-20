a26c77143a406ad77b870ada47450d5e1d436669
Benson Yuen <byuen@my.hpu.edu>
2019-09-19 16:07:29 -1000

    Variables print

--- hw03/readPPM.cc
+++ hw03/readPPM.cc
@@ -1,27 +1,36 @@
 #include "readPPM.h"
 #include <cstdio>
 //#include <cstdlib>
+
 unsigned char* readPPM(const char *fileName, int *w, int *h){
 
-    FILE* fp = fopen("fileName", "rb");
-    char p6[3];
-    fscanf(fp, "%s", p6);
-    if ((p6[0] != 'P') || (p6[1] != '6')){
+    FILE* fp = fopen(fileName, "rb");
+    char pSix[3];
+    fscanf(fp, "%s\n", pSix);
+    if ((pSix[0] != 'P') || (pSix[1] != '6')){
     	printf("%s\n", "Not a PPM image");
     }
 
-    fscanf(fp, "%d", w);
-    fscanf(fp, "%d", h);
+    fscanf(fp, "%d\n", w);
+    fscanf(fp, "%d\n", h);
 
-    // int *max;
-    // fscanf(fp, "%d", max);
+    int max;
+    fscanf(fp, "%d\n", &max);
 
-    return nullptr;
+    printf("P6: %s\n", pSix);
+    printf("Width: %d\n", *w);
+    printf("Height: %d\n", *h);
+    printf("Max: %d\n", max);
 
-	//unsigned char* pixels = new unsigned char[];
-    //return pixels;
+    int size = (*w * *h);
+    printf("Size: %d\n", size);
+    unsigned char* pixels = new unsigned char[size];
 
-	// while(fscanf(fp, "%d", &value)!= EOF){
-	// 	printf("%s", value);
-	// }
+    fclose(fp);
+
+    //return nullptr;
+
+    return pixels;
+
+
 }
--- hw03/readPPM.h
+++ hw03/readPPM.h
@@ -2,7 +2,6 @@
 #ifndef READPPM_H
 #define READPPM_H
 
-// this is the declaration of the swap function
 unsigned char* readPPM(const char* fileName, int *w, int *h);
 
 #endif
