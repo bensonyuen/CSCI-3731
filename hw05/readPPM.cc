00233995e536a79cc8719f40be2b4cbc7cc66a5e
Benson Yuen <byuen@my.hpu.edu>
2019-10-01 21:20:46 -1000

    initialization

---
+++ hw05/readPPM.cc
@@ -0,0 +1,32 @@
+#include "readPPM.h"
+#include <cstdio>
+
+unsigned char* readPPM(const char *fileName, int *w, int *h){
+
+    FILE* fp = fopen(fileName, "rb");
+    char pSix[3]; //array to read P6
+    fscanf(fp, "%s\n", pSix);
+    if ((pSix[0] != 'P') || (pSix[1] != '6')){
+        printf("%s\n", "Not a PPM image");
+    }
+
+    fscanf(fp, "%d\n", w); //reads width
+    fscanf(fp, "%d\n", h); //reads height
+
+    int max;
+    fscanf(fp, "%d\n", &max); //reads max
+
+    printf("Width: %d\n", *w);
+    printf("Height: %d\n", *h);
+
+    unsigned char* pixels = new unsigned char[*w * *h * 3];
+
+    fread(pixels, sizeof(*pixels), (*w * *h * 3), fp);
+    
+    fclose(fp);
+
+    
+
+    return pixels;
+
+}
---
+++ hw05/readPPM.h
@@ -0,0 +1,7 @@
+
+#ifndef READPPM_H
+#define READPPM_H
+
+unsigned char* readPPM(const char* fileName, int *w, int *h);
+
+#endif
