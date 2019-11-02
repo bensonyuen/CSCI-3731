eeaaf605140bd26ec14fe59cbd8f5c6df693a16d
Benson Yuen <byuen@my.hpu.edu>
2019-11-01 20:12:40 -1000

    Fixed writePPM

--- hw08/Image.h
+++ hw08/Image.h
@@ -4,14 +4,18 @@
 
 class Image{
 	private: 
-		unsigned char* pixels = nullptr;
+		
+		// int width = 650;
+		// int height = 652;
 
 	public: 
 		Image();
+		unsigned char* pixels = nullptr;
 		int w;
 		int h;
 		void readPPM(const char* fileName);
 		void writePPM(const char *fileName, Image i);
+		// void write_JPEG_file (const char *filename, int quality, Image i);
 		virtual ~Image();
 
 	};
--- hw08/main.cc
+++ hw08/main.cc
@@ -3,8 +3,13 @@
 int main() {
 	int w = 0;
 	int h = 0;
+	int quality = 255;
 	Image myImage;
 	myImage.readPPM("test.ppm");
-	Image writtenImage;
-	writtenImage.writePPM("test2.ppm", myImage);
+
+	Image writtenPPMImage;
+	writtenPPMImage.writePPM("test2.ppm", myImage);
+
+	// Image writtenJPEGImage;
+	// writtenJPEGImage.write_JPEG_file("test3.jpeg", quality, myImage);
 } // end of main
